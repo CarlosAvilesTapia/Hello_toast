b9cca9ef7285e03c79c6cbab22acf156e5942464
CAT <cavilest@gmail.com>
2023-06-19 20:46:42 -0400

    Ejercicio individual 5. - Diseño completo. - Se agrega funcionaliad al botón "Show toast"

--- app/src/main/java/com/example/tapcounter/MainActivity.java
+++ app/src/main/java/com/example/tapcounter/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.tapcounter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +14,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initListeners();
     }
 
+    private void initListeners() {
+        Button toast_button = findViewById(R.id.toast_button);
+        Button count_button = findViewById(R.id.count_button);
+        TextView counter = findViewById(R.id.count_text);
+
+        toast_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getBaseContext(),"Hello toast!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
 }
